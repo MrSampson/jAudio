3e963a8a7a1bdb5ae9dfc46d803af5db087e8728
Oliver Sampson <olsam@quickaudio.com>
2015-07-16 22:18:25 +0200

    Refactoring.

--- src/net/sf/jaudio/FeatureExtractor/AudioFeatures/Compactness.java
+++ src/net/sf/jaudio/FeatureExtractor/AudioFeatures/Compactness.java
@@ -23,85 +23,64 @@ import net.sf.jaudio.FeatureExtractor.ACE.DataTypes.FeatureDefinition;
  * @author Cory McKay
  */
 public class Compactness extends FeatureExtractor {
-	/* CONSTRUCTOR ************************************************************* */
+   
+    /**
+     * Basic constructor that sets the definition and dependencies (and their
+     * offsets) of this feature.
+     */
+    public Compactness() {
+        String name = "Compactness";
+        String description = "A measure of the noisiness of a signal. "
+                + "Found by comparing the components of a window's "
+                + "magnitude spectrum with the magnitude spectrum "
+                + "of its neighbouring windows.";
+        boolean is_sequential = true;
+        int dimensions = 1;
+        this.definition = new FeatureDefinition(name, description, is_sequential,
+                dimensions);
 
-	/**
-	 * Basic constructor that sets the definition and dependencies (and their
-	 * offsets) of this feature.
-	 */
-	public Compactness() {
-		String name = "Compactness";
-		String description = "A measure of the noisiness of a signal. "
-				+ "Found by comparing the components of a window's "
-				+ "magnitude spectrum with the magnitude spectrum "
-				+ "of its neighbouring windows.";
-		boolean is_sequential = true;
-		int dimensions = 1;
-		definition = new FeatureDefinition(name, description, is_sequential,
-				dimensions);
+        this.dependencies = new String[1];
+        this.dependencies[0] = "Magnitude Spectrum";
 
-		dependencies = new String[1];
-		dependencies[0] = "Magnitude Spectrum";
+        this.offsets = new int[1];
+        this.offsets[0] = 0;
+    }
 
-		offsets = new int[1];
-		offsets[0] = 0;
-	}
+    /**
+     * {@inheritDoc}
+     * 
+     * <p>
+     * In the case of this feature, the sampling_rate parameter is ignored.
+     * </p>
+     * 
+     * @author Daniel McEnnis 05-07-05 checks for degenerate case where
+     *         magnitude spectrum entry is exactly zero - skips these values
+     */
+    @Override
+    public double[] extractFeature(double[] samples, double sampling_rate,
+            double[][] other_feature_values) throws Exception {
+        double[] mag_spec = other_feature_values[0];
+        double compactness = 0.0;
+        for (int i = 1; i < mag_spec.length - 1; i++) {
+            if ((mag_spec[i - 1] > 0.0) && (mag_spec[i] > 0.0)
+                    && (mag_spec[i + 1] > 0.0)) {
+                compactness += Math
+                        .abs(20.0
+                                * Math.log(mag_spec[i])
+                                - 20.0
+                                * (Math.log(mag_spec[i - 1])
+                                        + Math.log(mag_spec[i]) + Math
+                                            .log(mag_spec[i + 1])) / 3.0);
+            }
+        }
 
-	/* PUBLIC METHODS ********************************************************* */
+        double[] result = new double[1];
+        result[0] = compactness;
+        return result;
+    }
 
-	/**
-	 * Extracts this feature from the given samples at the given sampling rate
-	 * and given the other feature values.
-	 * <p>
-	 * In the case of this feature, the sampling_rate parameter is ignored.
-	 * <p>
-	 * Daniel McEnnis 05-07-05 checks for degenerate case where magnitude
-	 * spectrum entry is exactly zero - skips these values
-	 * 
-	 * @param samples
-	 *            The samples to extract the feature from.
-	 * @param sampling_rate
-	 *            The sampling rate that the samples are encoded with.
-	 * @param other_feature_values
-	 *            The values of other features that are needed to calculate this
-	 *            value. The order and offsets of these features must be the
-	 *            same as those returned by this class's getDependencies and
-	 *            getDependencyOffsets methods respectively. The first indice
-	 *            indicates the feature/window and the second indicates the
-	 *            value.
-	 * @return The extracted feature value(s).
-	 * @throws Exception
-	 *             Throws an informative exception if the feature cannot be
-	 *             calculated.
-	 */
-	public double[] extractFeature(double[] samples, double sampling_rate,
-			double[][] other_feature_values) throws Exception {
-		double[] mag_spec = other_feature_values[0];
-		double compactness = 0.0;
-		for (int i = 1; i < mag_spec.length - 1; i++) {
-			if ((mag_spec[i - 1] > 0.0) && (mag_spec[i] > 0.0)
-					&& (mag_spec[i + 1] > 0.0)) {
-				compactness += Math
-						.abs(20.0
-								* Math.log(mag_spec[i])
-								- 20.0
-								* (Math.log(mag_spec[i - 1])
-										+ Math.log(mag_spec[i]) + Math
-										.log(mag_spec[i + 1])) / 3.0);
-			}
-		}
-
-		double[] result = new double[1];
-		result[0] = compactness;
-		return result;
-	}
-
-	/**
-	 * Create an identical copy of this feature. This permits FeatureExtractor
-	 * to use the prototype pattern to create new composite features using
-	 * metafeatures.
-	 */
-	public Object clone() {
-		return new Compactness();
-	}
+    @Override
+    public Object clone() {
+        return new Compactness();
+    }
 }
